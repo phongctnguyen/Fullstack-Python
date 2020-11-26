b15e1ec759021f6bb08a057c286d13cf91587a11
phongctnguyen <phongctnguyen@gmail.com>
2020-11-26 23:27:40 +0700

    finish day 1 and start day 2

---
+++ Day_2/main.py
@@ -0,0 +1,31 @@
+'''
+Primitive Data Types
+String - Integer - Float - Boolean
+'''
+
+#String
+# print('Hello')
+# print('Hello'[4])
+
+#Integer
+# print(2 + 6)
+# print(123_456_789)
+
+#Float
+# print(3.14)
+
+# Boolean
+# print(True)
+# print(False)
+
+'''
+Type Error, Type Checking and Type Conversion
+'''
+
+# len(123) - TypeError
+
+# num_char = 10
+# new_num_char = str(num_char)
+# print('Hello ' + new_num_char)
+
+# print(type('hello'))
