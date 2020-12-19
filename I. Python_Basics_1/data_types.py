9f49f187be18387f298acdb52e7ffb2fe1f72829
phongctnguyen <phongctnguyen@gmail.com>
2020-12-20 02:35:20 +0700

    start python course

---
+++ I. Python_Basics_1/data_types.py
@@ -0,0 +1,23 @@
+'''
+Fundamental Data Types
+'''
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+'''
+Classes -> custom types
+'''
+
+'''
+Specialized Data Types
+'''
+
+'''
+None
+'''
---
+++ I. Python_Basics_1/numbers.py
@@ -0,0 +1,17 @@
+'''
+Numbers - int and float
+'''
+
+print(2 + 4)
+print(2 - 4)
+print(2 * 4)
+
+print(2 ** 3);
+print(6 // 4);
+print(6 % 4);
+
+print(type(2 / 4)) # float
+
+# math functions
+print(round(3.5454, 2))
+print(abs(-67))
